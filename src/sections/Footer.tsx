2d4c6f1eb9597cf811af2cf8fa41264b330e0c88
Farda Safari <fardasafari@gmail.com>
2024-09-17 17:46:34 +0200

    footer width

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -14,7 +14,7 @@ const footerLinks = [
 export const Footer = () => {
   return (
     <footer className="relative -z-10 overflow-x-clip">
-      <div className="-z-10 absolute h-[500px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(80%_80%_at_bottom_center,black,transparent)]"></div>
+      <div className="-z-10 absolute h-[500px] w-[100%] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(80%_80%_at_bottom_center,black,transparent)]"></div>
       <div className="container pb-8">
         <div className="border-t border-white/15 py-6 text-sm flex gap-8 flex-col md:flex-row md:justify-between items-center">
           <div className="text-white/40">&copy; 2024. Safari Farda - Tous droits réservés.
